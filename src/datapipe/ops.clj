1097a669313e30ee33706b989425984d1fa1f96b
Frederik De Bleser <frederik@debleser.be>
2014-03-24 22:13:09 +0100

    Improve the documentation for the ops.

--- src/datapipe/ops.clj
+++ src/datapipe/ops.clj
@@ -2,45 +2,46 @@
   (require [clojure.java.io :as io]
            [clojure.string :as s]))
 
-; Filter operations
 (defn sample
-  "Keep only a number of elements, selected randomly."
+  "Reduce the size of the file by randomly selecting lines from the file.
+  The percentage is a value between 0 and 1. At 0, no rows are kept.
+  At 1, all rows are kept. At 0.5, about 50% of all rows are kept.
+  Example: sample 0.01"
   [percentage coll]
   (filter (fn [x] (< (Math/random) percentage)) coll))
 
 
 (defn select
-  "Select a set of keys."
+  "Select the rows where the selector matches.
+  Example: select :key \"A\"
+  Example: select :key [\"a\" \"b\" \"c\"]
+  Example (select only word characters): select :key #\"\\w\""
   [key selector coll]
   (let [pred
-        (cond (string? selector) #(= (% key))
-              (sequential? selector) #(some (set [(% key)]) selector)
-              (instance? java.util.regex.Pattern selector) #(re-matches selector (str (% key)))
+        (cond (string? selector)
+              #(= (% key))
+              (sequential? selector)
+              #(some (set [(% key)]) selector)
+              (instance? java.util.regex.Pattern selector)
+              #(re-matches selector (str (% key)))
               )]
     (filter pred coll)))
 
 (defn lookup
-  "Keep only the given column."
+  "Keep only the given column.
+  Example: lookup :time"
   [key coll]
   (map (fn [row] {key (get row key)}) coll))
 
-; Keep only the ones where (key coll) = val.
-; Inputs are strings, so values should also be strings.
-(defn equals [key val coll]
+(defn equals
+  "Keep only the rows where the key in the collection matches the value.
+  Example: equals :key \"A\""
+  [key val coll]
   (filter #(= (% key) val) coll))
 
-(defn only-letters
-  "Filter the keys with only letters."
-  [coll]
-  (filter (fn [row]
-            (let [val (get row "key")]
-              (and
-               (not (nil? val))
-               (re-matches #"\w" (get row "key")))))
-          coll))
-
-(defn freq-map
-  "Count the occurences of a value for the given key and return a map with key=count."
+(defn- freq-map
+  "Count the occurences of a value for the given key
+  and return a map with key=count."
   ([key coll]
   (reduce
      (fn [cnts row]
@@ -56,8 +57,10 @@
    {}
    coll)))
 
-(defn freqs
-  "Count the occurences of a value for the given key and return a list with key,count."
+(defn- freqs
+  "Count the number of time a value in the given column occurs.
+  You can also specify two columns; in this case both values have to match.
+  Example: freqs :x :y"
   ([key coll]
   (let [fm (freq-map key coll)]
     (map (fn [[kv amt]] {key kv :amount amt}) fm)))
@@ -66,7 +69,11 @@
      (map (fn [[[kv1 kv2] amt]] {k1 kv1 k2 kv2 :amount amt}) fm))))
 
 
-(defn parse-float
+(defn- parse-float
+  "Convert a string to a floating point number.
+  If the string is not a number, return 0, or a default value
+  of your choosing.
+  If the value is already a number, it is returned as-is."
   ([s] (parse-float s 0.0))
   ([s default]
    (if (number? s)
@@ -76,7 +83,10 @@
        (catch Exception e default)))))
 
 (defn scale
-  "Reduce the size of the input keys. This assumes the keys are numbers."
+  "Multiply the values in the given column.
+  The column needs to contain numbers.
+  Numbers are rounded to the nearest integer.
+  Example: scale :x 0.1"
   [key scale coll]
   (map (fn [row]
          (let [v (parse-float (get row key ""))
@@ -101,10 +111,5 @@
                 x2 (parse-float (pt2 :x))
                 y2 (parse-float (pt2 :y))
                 dist (get pt1 :distance 0)]
-            {:x x2 :y y2 :distance (+ dist (geo-distance x1 y1 x2 y2))})) coll)])
-
-
-;(def pts [{:x 0 :y 0} {:x 10 :y 10} {:x 20 :y 20}])
-;(distance pts)
-
-
+            {:x x2 :y y2
+             :distance (+ dist (geo-distance x1 y1 x2 y2))})) coll)])
