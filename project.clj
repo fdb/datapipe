0bee38eba1909de9ea076bc3d3bcfce447ad65dc
Frederik De Bleser <frederik@debleser.be>
2015-06-09 22:29:07 +0200

    Upgrade seesaw.

--- project.clj
+++ project.clj
@@ -5,4 +5,4 @@
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :main datapipe.gui
   :dependencies [[org.clojure/clojure "1.6.0"]
-                 [seesaw "1.4.4"]])
+                 [seesaw "1.4.5"]])
