306ffc55b515a178ee56d86ac669fdcd2bb72165
Frederik De Bleser <frederik@debleser.be>
2015-06-09 22:26:40 +0200

    Upgrade Clojure.

--- project.clj
+++ project.clj
@@ -4,5 +4,5 @@
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :main datapipe.gui
-  :dependencies [[org.clojure/clojure "1.5.1"]
+  :dependencies [[org.clojure/clojure "1.6.0"]
                  [seesaw "1.4.4"]])
